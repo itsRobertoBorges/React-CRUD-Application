fc2f320bde6fcf87aa492aa231e8d9b2f854dd58
robertoborges98 <robbyborges98@outlook.com>
2021-12-19 00:46:05 -0500

    changes to create and submit functions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    
+    this.state ={
+      title :  "A Simple ReactJS CRUD Application",
+      employeeData : [],
+      act : 0,
+      index : ''
+    }
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    let employeeData = this.state.employeeData;
+    let name = this.refs.txtName.value;
+    let lname = this.refs.txtLname.value;
+    let age = this.refs.txtAge.value
+
+    if(this.state.act === 0){
+
+      let newEmployee = {
+        "name": name,
+        "lname": lname,
+        "age": age
+      }
+      employeeData.push(newEmployee)
+    }
+    else{
+      let index = this.state.index
+      employeeData[index].name = name;
+      employeeData[index].lname = lname;
+      employeeData[index].age = age;
+    }
+    
+    this.setState({
+      employeeData : employeeData,
+      act : 0
+    })
+
+    this.refs.myForm.reset();
+  } 
+
+  handleEdit = (i) => {
+    let employeeData = this.state.employeeData[i];
+    this.refs.txtName.value = this.state.employeeData.name;
+    this.refs.txtLname.value = this.state.employeeData.lname;
+    this.refs.txtAge.value = this.state.employeeData.age;
+
+    this.setState({
+      employeeData : employeeData,
+      act : 1,
+      index : i
+    })
+    console.log(employeeData)
+  }
+
+  handleDelete = (i) => {
+    let employeeData = this.state.employeeData;
+    employeeData.splice(i,1);
+
+    this.setState({
+      employeeData : employeeData
+    })
+  }
+  render() {
+    let employeeData = this.state.employeeData;
+    return (
+      <div>
+        <h1>{this.state.title}</h1>
+      <form ref="myForm">
+        <label>Name</label>
+        <input type="text" ref="txtName" placeholder="Enter Name"/>
+        <label>Last Name</label>
+        <input type="text" ref="txtLname" placeholder="Enter Last Name"/>
+        <label>Age</label>
+        <input type="text" ref="txtAge" placeholder="Enter Age"/>
+        <button onClick={e => this.handleSubmit(e)}>Save</button>
+      </form>
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Age</th>
+          <th>Last Name</th>
+        </tr>
+        {
+        employeeData.map( (data, i) =>
+        <tr key={i}>
+            <td>{data.name}</td>
+            <td>{data.age}</td>
+            <td>{data.lname}</td>
+           <td>
+              <button onClick={i => this.handleEdit(i)}>Edit</button>
+            </td>
+            <td>
+              <button onClick={i => this.handleDelete(i)}>Delete</button>
+            </td>
+        </tr>)
+        }
+      </table>
+      </div>
+    );
+  }
 }
 
 export default App;
